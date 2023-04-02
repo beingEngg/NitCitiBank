6cb2608ed0dc0d80a575412744679bb65826f0cc
DEV1 <DEV1@gmail.com>
2023-04-02 16:43:15 +0530

    added main() and getSysDate()

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -1,5 +1,12 @@
 package com.nt.beans;
 
+
+import java.time.LocalDate;
+
+
 public class Utility {
+	public LocalDate getSysDate() {
+		return LocalDate.now();
+	}
 
 }
--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -1,5 +1,11 @@
 package com.nt.test;
 
+import com.nt.beans.Utility;
+
 public class Test {
+	public static void main(String [] args) {
+		Utility uti=new Utility();
+		System.out.println(uti.getSysDate());
+	}
 
 }
